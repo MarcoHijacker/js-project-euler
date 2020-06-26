4d91db37a7833bd8cdc6add3f658c53139ddae40
Marco <marco.longo@hotmail.it>
2020-06-26 18:01:45 +0200

    Ex.ID 1 Risolto!

---
+++ id1-multiples3and5/js/script.js
@@ -0,0 +1,15 @@
+// Ex.ID 1 - Multiples of 3 and 5
+
+var sum = 0;
+
+for (var i = 1; i < 1000; i++) {
+  if (i%3 == 0 && i%5 == 0) {
+    sum += i;
+  } else if (i%3 == 0) {
+    sum += i;
+  } else if (i%5 == 0) {
+    sum += i;
+  }
+}
+
+console.log(sum);
